4d0f27c2973aa3672ff9f20a62908f5618d4b0f5
mariah <mariah@gardziola.com>
2019-06-11 17:09:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash {"Name" => "Mariah", "Age" => 24}  
+my_hash {"Name" => "Mariah", "Age" => "Twenty-Four"}  
 end
 
 
